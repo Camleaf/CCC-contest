377869b5a820b1391ad4dbcf1a3924282e719b0c
Camleaf <bdaace09@gmail.com>
2026-02-04 09:12:25 -0500

    more updates

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 # idea for optimization. only store top two layers. will need to fix system though
 import sys
-print = sys.stdout.write
+#print = sys.stdout.write
 input = sys.stdin.readline
 #solves for first 5 part marks
 
 N,M,Q = list(map(int,input().split(" ")))
 
 highest_prettiness = [0 for _ in range(M)]
-lowest_high:int = 0
 greatest_second:int = 0
 
 for i in range(N):
     c,p = list(map(int,input().split(" ")))
     
     if (highest_prettiness[c-1] < p):
+        greatest_second = max(greatest_second,highest_prettiness[c-1])
         highest_prettiness[c-1] = p
-
-        if (lowest_high == 0 or lowest_high > p):
-            lowest_high = p
     
     elif (greatest_second < p):
         greatest_second = p
 
+
+lowest_high = min(highest_prettiness)
+#print(lowest_high, greatest_second, highest_prettiness)
 total = sum(highest_prettiness) - lowest_high + max(lowest_high,greatest_second)
 
-_=print(str(total)+"\n")
+_=print(str(total))
